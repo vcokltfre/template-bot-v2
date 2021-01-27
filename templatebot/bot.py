5b1c96463476e7603fcce8c3c652eced1e934080
vcokltfre <vcokltfre@gmail.com>
2021-01-27 03:30:18 +0000

    feat: add logging to main bot

--- templatebot/bot.py
+++ templatebot/bot.py
@@ -1,15 +1,17 @@
 from discord.ext import commands
 from typing import List
 
+from .utils.logger import WebhookLogger, LogLevel
+
 
 class Bot(commands.Bot):
     """A subclassed version of commands.Bot"""
 
-    def __init__(self, name: str, command_prefix, *args, **kwargs):
+    def __init__(self, name: str, command_prefix, logging_url: str = None, loglevel: int = LogLevel.INFO, *args, **kwargs):
         self.name = name
         super().__init__(command_prefix=command_prefix, *args, **kwargs)
 
-        # TODO: Logging initialisation
+        self.logger = WebhookLogger(name=name, url=logging_url, level=loglevel)
 
     def load_initial_cogs(self, cogs: List[str]):
         """Loads the initial cogs"""
@@ -21,7 +23,7 @@ class Bot(commands.Bot):
                 super().load_extension(cog)
                 success += 1
             except Exception as e:
-                # TODO: Log error
+                self.logger.error_s(f"Cog {cog} experienced an error during loading: {e}")
                 failed += 1
 
-        # TODO: Log cog load stats
+        self.logger.info_s(f"Cog loading complete! (Total: {success + failed} | Loaded: {success} | Failed: {failed})")
